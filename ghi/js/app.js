7a5f3eb74fa0811ab818083fa25047ef82244571
Hack Reactor <>
2022-04-25 20:26:34 -0500

    Remove unintentional JS.

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,10 +1,4 @@
-window.addEventListener('DOMContentLoaded', async () => {
+window.addEventListener('DOMContentLoaded', () => {
 
-  const url = 'http://localhost:8000/api/conferences/';
-  const response = await fetch(url);
-  if (response.ok) {
-    const data = await response.json();
-    console.log(data);
-  }
 
 });
